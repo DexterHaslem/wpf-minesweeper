d6ed9581d1ac3818ce47aa9e4bfa6b9748efe34c
Dexter Haslem <dexter.haslem@gmail.com>
2014-05-08 23:44:18 -0600

    initial commit

---
+++ wpf-minesweeper/GameRules.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace wpf_minesweeper
+{
+    internal enum GameState
+    {
+        Unset,
+        Initialized,
+        InProgress,
+        Bombed,
+        Winner,
+    }
+
+    internal class GameRules
+    {
+        private Stopwatch       _elapsedTime;
+
+        // config state (cannot be changed w/o creating a new game)
+        internal int BombCount    { get; private set; }
+        internal int Rows         { get; private set; }
+        internal int Columns      { get; private set; }
+
+        internal TimeSpan ElapsedGameTime
+        {
+            get {  return _elapsedTime != null ? _elapsedTime.Elapsed : new TimeSpan(); }
+        }
+
+        internal GameState GameState { get; private set; }
+        internal ObservableCollection<FieldSpot> GameField { get; private set; }
+
+        //private static FieldSpot FieldAt(IEnumerable<FieldSpot> fields, int row, int col) { return fields != null ? fields.FirstOrDefault(f => f.Row == row && f.Column == col) : null;  }
+        //private FieldSpot FieldAt(int row, int col) { return GameField != null ? GameField.FirstOrDefault(f => f.Row == row && f.Column == col) : null;  }
+
+        private static void SpreadSomeBombs(GameRules gameRules)
+        {
+            Debug.Assert(gameRules != null);
+            Random rand = new Random((int)DateTime.Now.Ticks); // loss of precision is intentional here
+            List<Point> usedLocations = new List<Point>();
+
+            for (int i = 0; i < gameRules.BombCount; i++)
+            {
+                Point newBombPoint;
+                while (usedLocations.Contains(newBombPoint = new Point(rand.Next(gameRules.Rows), rand.Next(gameRules.Columns))) && usedLocations.Count < gameRules.BombCount)
+                {
+                    // generate a unique point
+                }
+
+                int scalarX = (int)newBombPoint.X;
+                int scalarY = (int)newBombPoint.Y;
+                usedLocations.Add(newBombPoint);
+                var field =  gameRules.GameField.FirstOrDefault(f => f.Row == scalarX && f.Column == scalarY);
+                if (field != null)
+                    field.IsBomb = true;
+            }
+        }
+
+        private static IEnumerable<FieldSpot> CreateNewField(int rows, int cols)
+        {
+            for (int x = 0; x < rows; x++)
+                for (int y = 0; y < cols; y++)
+                    yield return new FieldSpot { Row = x, Column = y };
+        }
+
+        private GameRules() { }
+
+        internal static GameRules NewGame(int rows, int cols, int bombCount)
+        {
+            // lets fill out a new chunk of state for someone
+            var newGameRules = new GameRules 
+            {
+                GameField  = new ObservableCollection<FieldSpot>(CreateNewField(rows, cols)),
+                BombCount   = bombCount, 
+                Rows        = rows, 
+                Columns     = cols 
+            };
+            SpreadSomeBombs(newGameRules);
+            newGameRules.GameState = GameState.Initialized;
+            return newGameRules;
+        }
+
+        private int GetNeighborBombCount(FieldSpot spot)
+        {
+            if (spot.CachedBombNeighborCount != null)
+                return spot.CachedBombNeighborCount.Value;
+            return 0;
+        }
+
+        internal void SetTextForField(FieldSpot spot)
+        {
+            // note ordre is somewhat important
+            if (spot.IsBomb)
+                spot.Text = "X";
+            else if (spot.IsFlagged)
+                spot.Text = "F";
+            else if (spot.IsCleared)
+                spot.Text = "C";
+            else 
+                spot.Text = string.Empty;
+        }
+
+        internal void ClearSpot(FieldSpot spot)
+        {
+            spot.IsCleared = true;
+            SetTextForField(spot);
+            UpdateGameState();
+        }
+
+        internal void FlagSpot(FieldSpot spot)
+        {
+            spot.IsFlagged = !spot.IsFlagged;
+            SetTextForField(spot);
+            UpdateGameState();
+        }
+
+        private void UpdateGameState()
+        {
+            // if any bombs = pressed, user lost, if all bomb spots = flagged and all non bomb spots = cleared, user wins
+            if (GameState < GameState.InProgress)
+            { 
+                GameState = wpf_minesweeper.GameState.InProgress;
+                _elapsedTime = new Stopwatch();
+                _elapsedTime.Start();
+                // note: continue to run game logic here, it coulda been their first click
+            }
+
+            if (GameField.Any(f => f.IsCleared && f.IsBomb))
+            {
+                // user dun goofed
+                GameState = GameState.Bombed;
+                _elapsedTime.Stop();
+                return;
+            }
+
+            if (GameField.Where(f => f.IsBomb).All(f => f.IsFlagged) && GameField.Where(f => !f.IsBomb).All(f => f.IsCleared))            
+            {
+                GameState = GameState.Winner;
+                _elapsedTime.Stop();
+            }
+            else 
+                GameState = GameState.InProgress;
+        }
+
+        internal int GetClearedCount()      {   return GameField != null ? GameField.Where(f => f.IsCleared).Count() : 0;                   }
+        internal int GetRemainingCount()    {   return  GameField != null ? GameField.Count - GetClearedCount() - GetFlaggedCount() : 0;    }
+        internal int GetFlaggedCount()      {   return  GameField != null ? GameField.Where(f => f.IsFlagged).Count() : 0;                  }
+    }
+}
---
+++ wpf-minesweeper/ViewModel.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace wpf_minesweeper
+{
+    public class NotBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool) value ? Visibility.Collapsed : Visibility.Visible;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+
+    public class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName]string name = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class FieldSpot : NotifyPropertyChangedBase
+    {
+        private string  _text;
+        private bool    _isBomb;
+        private bool    _isCleared;
+        private bool    _isFlagged;
+
+        public bool IsCleared
+        {
+            get { return _isCleared; }
+            set
+            {
+                if (_isCleared == value)
+                    return;
+                _isCleared = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsBomb                      
+        { 
+            get { return _isBomb; }
+            set
+            {
+                if (_isBomb == value)
+                    return;
+                _isBomb = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsFlagged
+        {
+            get { return _isFlagged; }
+            set
+            {
+                if (_isFlagged == value)
+                    return;
+                _isFlagged = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (_text == value)
+                    return;
+                _text = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // gamestate stuff only
+        public int?     CachedBombNeighborCount     { get; set; }
+        
+        public int      Row                         { get; set; }
+        public int      Column                      { get; set; }
+        public Point    Point { get { return new Point(Row, Column); } }
+    }
+
+    public class MineSweeperViewModel : NotifyPropertyChangedBase
+    {
+        private TimeSpan        _currentTime;
+        private int             _columns;
+        private int             _flaggedCount;
+        private int             _difficulty;
+        private int             _clearedCount;
+        private int             _remainingCount;
+        private bool            _isGameConfigEnabled;
+        private MainView        _owner;
+        private int             _rows;
+        private int             _bombCount;
+        private DispatcherTimer _uiTimer;
+        private GameRules       _gameRules;
+
+        private const int UI_UPDATE_RATE_MS = 50;
+
+        //public static int MaxDifficulty = 10; // mostly for granularity of the slider on ui (scaled to percent of row*col)
+       // public static int MaxFieldSize = 20; // max size of a row or column
+
+        public int FlaggedCount
+        {
+            get { return _flaggedCount; }
+            set
+            {
+                if (_flaggedCount == value)
+                    return;
+                _flaggedCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int ClearedCount
+        {
+            get { return _clearedCount; }
+            set
+            {
+                if (_clearedCount == value)
+                    return;
+                _clearedCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Difficulty
+        {
+            get { return _difficulty; }
+            set
+            {
+                if (_difficulty == value)
+                    return;
+                _difficulty = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsGameConfigEnabled
+        {
+            get { return _isGameConfigEnabled; }
+            set
+            {
+                if (_isGameConfigEnabled == value)
+                    return;
+                _isGameConfigEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int RemainingCount
+        {
+            get { return _remainingCount; }
+            set
+            {
+                if (_remainingCount == value)
+                    return;
+                _remainingCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public TimeSpan CurrentTime
+        {
+            get { return _currentTime; }
+            set
+            {
+                if (_currentTime == value)
+                    return;
+                _currentTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public static RoutedCommand FieldSelectedCommand    = new RoutedCommand("FieldSelected", typeof(MineSweeperViewModel));
+        public static RoutedCommand FieldFlaggedCommand     = new RoutedCommand("FieldFlagged", typeof(MineSweeperViewModel));
+        public static RoutedCommand StartGameCommand        = new RoutedCommand("StartGame", typeof(MineSweeperViewModel));
+        public static RoutedCommand ResetGameCommand        = new RoutedCommand("ResetGame", typeof(MineSweeperViewModel));
+
+        public int BombCount
+        {
+            get { return _bombCount; }
+            set
+            {
+                if (_bombCount == value)
+                    return;
+                _bombCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Columns
+        {
+            get { return _columns; }
+            set
+            {
+                if (_columns == value)
+                    return;
+                _columns = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Rows
+        {
+            get { return _rows; }
+            set
+            {
+                if (_rows == value)
+                    return;
+                _rows = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MineSweeperViewModel(MainView owner)
+        {
+            _owner = owner;
+            _owner.CommandBindings.Add(new CommandBinding(FieldSelectedCommand, OnFieldSelected, CanFieldSelectExecute));
+            _owner.CommandBindings.Add(new CommandBinding(FieldFlaggedCommand, OnFieldFlagged, CanFieldSelectExecute));
+            _owner.CommandBindings.Add(new CommandBinding(StartGameCommand, OnStartGame));
+            _owner.CommandBindings.Add(new CommandBinding(ResetGameCommand, OnForceResetGame));
+            _uiTimer = new DispatcherTimer(TimeSpan.FromMilliseconds(UI_UPDATE_RATE_MS), DispatcherPriority.Background, OnUiUpdateTimerTick, _owner.Dispatcher);
+           
+            // give some defaults here for initial window load to not be empty
+            Difficulty = 1;
+            Rows = 8;
+            Columns = 8;
+            BombCount = 5;
+            _gameRules = GameRules.NewGame(Rows, Columns, BombCount);
+            // kick off a fresh game
+            ResetGame();
+        }
+
+        private void OnForceResetGame(object sender, ExecutedRoutedEventArgs e)
+        {
+            ResetGame();
+        }
+
+        private void OnStartGame(object sender, ExecutedRoutedEventArgs e)
+        {
+            BombCount = (int)((Rows * Columns) * (Difficulty / 10d));
+            _gameRules = GameRules.NewGame(Rows, Columns, BombCount);
+            // HACK: force update crap (as well as before timer starts)
+            OnPropertyChanged("MineField");
+            IsGameConfigEnabled = false;
+            CheckCurrentGameState();
+        }
+
+        public ObservableCollection<FieldSpot> MineField { get {  return _gameRules != null ? _gameRules.GameField : null; } }
+
+        private void CheckCurrentGameState()
+        {
+            if (!_uiTimer.IsEnabled)
+                _uiTimer.Start();
+            // user still chugging away, not yet interested in taking action
+            if (_gameRules.GameState != GameState.Winner && _gameRules.GameState != GameState.Bombed)
+                return;
+      
+            string message = (_gameRules.GameState == GameState.Winner ? "You won! great work mate." : "You lost! Terrible luck.") + "  Play a new game?";
+            if (MessageBox.Show(_owner, message, "Game over", MessageBoxButton.YesNo, MessageBoxImage.Question) != MessageBoxResult.Yes)
+            { 
+                _owner.Close();
+                return;
+            }
+
+            ResetGame();
+        }
+
+        private void ResetGame()
+        {
+            _uiTimer.Stop();
+            IsGameConfigEnabled = true;
+            CurrentTime = new TimeSpan();
+        }
+
+        private void OnUiUpdateTimerTick(object sender, EventArgs e)
+        {
+            // chop the milliseconds off
+            CurrentTime     = TimeSpan.FromSeconds((int)_gameRules.ElapsedGameTime.TotalSeconds);
+            ClearedCount    = _gameRules.GetClearedCount();
+            RemainingCount  = _gameRules.GetRemainingCount();
+            FlaggedCount    = _gameRules.GetFlaggedCount();
+        }
+
+        private void CanFieldSelectExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+            return;
+            /*
+            if (IsGameConfigEnabled)
+            {
+                e.CanExecute = false;
+                return;
+            }
+
+            var element = e.OriginalSource as FrameworkElement;
+            if (element == null)
+                return;
+            var fieldSpot = element.DataContext as FieldSpot;
+            e.CanExecute = fieldSpot != null && !fieldSpot.IsCleared && !fieldSpot.IsFlagged;*/
+        }
+
+        private void OnFieldFlagged(object sender, ExecutedRoutedEventArgs e)
+        {
+            var fieldSpot = e.Parameter as FieldSpot;
+            if (fieldSpot == null)
+                return;
+
+            _gameRules.FlagSpot(fieldSpot);
+            CheckCurrentGameState();
+        }
+
+        private void OnFieldSelected(object sender, ExecutedRoutedEventArgs e)
+        {
+            var fieldSpot = e.Parameter as FieldSpot;
+            if (fieldSpot == null)
+                return;
+            _gameRules.ClearSpot(fieldSpot);
+            CheckCurrentGameState();
+        }
+    }
+}
